bfea3063455ac1144c1ff707320a01d4921b01f7
Natalie G <natalieg@sparkcentral.com>
2018-03-19 18:54:11 -0700

    implemented backtracking mazeRunner method, renamed package.

--- src/main/java/com/algorithms/maze/MazeRunner.java
+++ src/main/java/com/algorithms/maze/MazeRunner.java
@@ -1,10 +1,13 @@
-package com.algorithms.bfs;
+package com.algorithms.maze;
 
 import com.google.common.collect.Lists;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/**
+ * example for BFS and backtracking(recursion) algorithms usage
+ */
 public class MazeRunner {
   static class Point {
     private final int x;
@@ -31,6 +34,10 @@ public class MazeRunner {
 
   /**
    * Finds the shortest path between two points using BFS algorithm (wave propagation)
+   * Problem definition:
+   *   Given a 2D map find the shortest distance from the starting point to the finish
+   *  point. You can only move horizontally or vertically. You cannot leave the
+   *  boundaries of the map.
    * @param start
    * @param end
    * @return
@@ -71,4 +78,22 @@ public class MazeRunner {
     //no path available from source to destination
     return -1;
   }
+
+  /**
+   * A Maze is given as N*N binary matrix of blocks where source block is the upper left most block i.e.,
+   * maze[0][0] and destination block is lower rightmost block i.e., maze[N-1][N-1]. A rat starts from source and has to reach destination.
+   * The rat can move only in two directions: forward and down.
+   * Solve with backtracking.
+   * @return true if there is a way out.
+   */
+  public boolean rescueRat(Point start, Point end){
+    if( start.getY() == end.getY() && start.getX() == end.getX()){
+      return true;
+    }
+    if( start.getX() >= maze[0].length || start.getY() >= maze.length || maze[start.getY()][start.getX()] == 0 ){
+      return false;
+    }
+    return rescueRat(new Point(start.getX(), start.getY()+1), end) || rescueRat(new Point(start.getX()+1, start.getY()), end);
+  }
+
 }
--- src/test/java/com/algorithms/maze/MazeRunnerTest.java
+++ src/test/java/com/algorithms/maze/MazeRunnerTest.java
@@ -1,8 +1,10 @@
-package com.algorithms.bfs;
+package com.algorithms.maze;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import com.algorithms.bfs.MazeRunner.Point;
+import com.algorithms.maze.MazeRunner.Point;
 import org.junit.Test;
 
 public class MazeRunnerTest {
@@ -19,5 +21,13 @@ public class MazeRunnerTest {
     final int[][] maze = new int[][]{{1,0,1},{1,0,1},{1,0,0}};
     MazeRunner mazeRunner = new MazeRunner(maze);
     assertEquals(-1, mazeRunner.findShortestPath(new Point(0,0), new Point(2,0)));
+  }
+
+  @Test
+  public void testRescueRat(){
+    final int[][] maze = new int[][]{{1,0,1},{1,0,1},{1,0,0}};
+    MazeRunner mazeRunner = new MazeRunner(maze);
+    assertFalse(mazeRunner.rescueRat(new Point(0,0), new Point(2,2)));
+    assertTrue(mazeRunner.rescueRat(new Point(2, 0), new Point(2, 1)));
   }
 }
